5995f129f77a09ad705a841604c6043acf815ea8
13560107927 <13560107927@139.com>
2023-06-16 12:02:24 +0800

    1、增加跨域配置； 2、修复Sweet-API调试日志无法输出问题； 3、web增加/sweet-api访问上下文；

---
+++ sweet-api-web/src/main/java/com/egrand/sweetapi/web/config/WebMvcConfiguration.java
@@ -0,0 +1,31 @@
+package com.egrand.sweetapi.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
+
+    /**
+     * 资源处理器
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html", "doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS).maxAge(3600);
+    }
+}
